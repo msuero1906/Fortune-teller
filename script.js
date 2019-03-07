f66998aa3ab6ed5e7abee9a8c87cb5c372210758
msuero1906 <43377325+msuero1906@users.noreply.github.com>
2019-03-07 08:20:20 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,8 @@
+$(".m").click(function(){
+  var age=$(".lol").val();
+    var season=$(".mel").val();
+    var name=$(".hi").val();
+
+$(".answer").text("In 3034, you will be hanging out with"+" "+ name+" "+"in" + " "+season);
+  });
+  
